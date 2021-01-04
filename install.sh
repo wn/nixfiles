faabd0a58b147c0f210dec265367d89ba8b4f1e8
Ang Wei Neng <weineng.a@gmail.com>
2021-01-04 22:26:49 +0800

    Update install script to auto download required programs

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 EXCLUDE=('README.md', 'install.sh', 'iterm', 'vimium_dvorak.txt', 'authorized_keys', 'config')
 
+if ! command -v zsh &> /dev/null
+then
+    echo '[+] Installing zsh'
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
 for config in *
 do
     if [[ ${EXCLUDE[*]} =~ $config ]]
@@ -21,6 +28,15 @@ done
 echo '[+] Dotfiles have been deployed'
 
 echo '[+] Fetching & Installing dependencies'
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh > /dev/null 2>&1
+# curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh > /dev/null 2>&1
+
+if ! command -v brew &> /dev/null
+then
+    echo '[+] Installing brew'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install zsh-syntax-highlighting zsh-autosuggestions z hub fzf
+fi
 
 echo '[+] Deployment complete'
+
+source ~/.zshrc
