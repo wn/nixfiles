9be7e17325150fbf8a58a20ea77961094ec899d6
Ang Wei Neng <weineng.a@gmail.com>
2025-01-10 23:00:45 -0500

    clean up

--- home.nix
+++ home.nix
@@ -6,42 +6,38 @@ in {
   nixpkgs.config.allowUnfree = true;
   fonts.fontconfig.enable = true;
 
-  # services.emacs = {
-  #    enable = true;
-  #    defaultEditor = true;
-  # };
-
   home = {
     username = "weineng";
     homeDirectory = "/Users/weineng";
     stateVersion = "20.09";
     packages = with pkgs; [
-      (python39.withPackages (p: with p; [
-        isort
+      (python3.withPackages (p: with p; [
         matplotlib
-        nose
         numpy
         pyflakes
+        pyright
         pygments
         pytest
         pylint
         flask
       ]))
 
-      nodePackages.pyright
       nodePackages.mathjax
 
       emacsPackages.compat
+      emacsPackages.pdf-tools
 
-      clang-tools
       bash
       bazel
-      ccls
+      cmake
+      llvm
       coreutils
       curl
       emacs29
       delta
       eza
+      glib
+      gbenchmark
       fd
       fzf
       flamegraph
@@ -49,6 +45,7 @@ in {
       qemu
       nasm
       git
+      clang-tools
       gnupg
       go
       go-outline
@@ -64,7 +61,6 @@ in {
       mplayer
       nixfmt
       nmap
-      pinentry
       pipenv
       ripgrep
       R
@@ -76,7 +72,6 @@ in {
       libGL
 
       # pdfviewer for emacs
-      emacsPackages.pdf-tools
       cairo
       libpng
       poppler
@@ -103,7 +98,7 @@ in {
   programs.git = {
     enable = true;
     userName = "Ang Wei Neng";
-    userEmail = "weineng@twosigma.com";
+    userEmail = "weineng.a@gmail.com";
     ignores = [
       "*.DS_Store"
       "*.DS_Store*"
@@ -116,11 +111,9 @@ in {
       ".clangd"
       ".gitmodules"
       ".projectile"
-      ".test-all.v1.sqlite3"
       ".testlist"
       ".tspkg"
       "compile_commands.json"
-      "compile_commands.zsh"
       "flycheck_*"
     ];
     aliases = {
@@ -128,7 +121,7 @@ in {
       root = "rev-parse --show-toplevel";
     };
     extraConfig = {
-      core ={
+      core = {
         autocrlf = false;
         editor = "emacs -nw -Q -f menu-bar-mode";
       };
@@ -149,38 +142,14 @@ in {
     enable = true;
     autocd = true;
     dotDir = ".config/zsh";
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     enableCompletion = true;
 
     shellAliases = {
       ls = "eza";
       l = "eza -l";
       la = "eza -la";
-
-      # git-meta aliases
-      gm = "git";
-      gco = "gm checkout";
-      gst = "gm status";
-      gcmsg = "gm commit -m";
-      gcb = "gm checkout -b";
-      gaa = "gm add $(gm root)";
-      ga = "gm add";
-      gcm = "gm checkout master";
-      gca = "gm commit --amend";
-      gd = "gm diff";
-      gaaa = "gaa && gm commit --amend --no-edit && gm push -f && tsdev pr update";
-      gp = "gm push -f && tsdev pr update";
-      gpsup = "gm push && tsdev pr create --jira";
-      gl = "gm log --decorate --graph";
-      gdc = "gm diff --cached";
-      gb = "gm branch";
-      gpu = "gm pull --rebase upstream master";
-      gba = "gm branch -a";
-      cdr = "builtin cd $(gm root)";
-      grhh = "gm reset --hard";
-      gss = "gm submodule status";
-      goc = "git-meta open -c $(git-meta rev-parse HEAD)";
 
       # nix-os alias
       rr = ''
@@ -189,19 +158,10 @@ in {
 
       em = "emacsclient -c -n &";
       e = "emacsclient -c -n";
-      doom = "gssproxy2 ~/.emacs.d/bin/doom";
+      doom = "~/.emacs.d/bin/doom";
 
       # Force g++ compiler to show all warnings and use C++20
       gpp = "g++ -Wall -Weffc++ -std=c++2a -Wextra -Wsign-conversion";
-
-      # two sigma specific alias
-      braindump = "builtin cd ~/.org/braindump && make";
-      bump = "cdr && builtin cd ts/mmia/bump/";
-      mra = "make realclean all";
-      mara = "make-all realclean all";
-      t = "./bin/tstest";
-      tsi = "cdr && builtin cd ts/tss/integration";
-      tt = "tsdev test";
     };
 
     plugins = with pkgs; [
@@ -215,16 +175,6 @@ in {
           sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
         };
       }
-      # {
-      #   name = "formarks";
-      #   src = fetchFromGitHub {
-      #     owner = "wfxr";
-      #     repo = "formarks";
-      #     rev = "8abce138218a8e6acd3c8ad2dd52550198625944";
-      #     sha256 = "1wr4ypv2b6a2w9qsia29mb36xf98zjzhp3bq4ix6r3cmra3xij90";
-      #   };
-      #   file = "formarks.plugin.zsh";
-      # }
       {
         name = "zsh-syntax-highlighting";
         src = fetchFromGitHub {
@@ -263,8 +213,10 @@ in {
           PATH="''${PATH:+"$PATH:"}$1"
         fi
       }
+
       export TERM=xterm-256color
       export TERMCAP=
+
       # set up pure
       autoload -U promptinit
       promptinit
@@ -288,24 +240,6 @@ in {
         ~/.emacs.d/bin/doom install
       fi
 
-      # two sigma specific stuff
-      if [[ $(hostname -d) = *twosigma.com ]]; then
-         source /nix/setup-nix.sh
-         export TZ='America/New_York';
-         export no_proxy="twosigma.com,*.twosigma.com,127.0.0.1,localhost";
-
-         pathadd "$HOME/.config/scripts"
-         pathadd "$HOME/dotfiles/.bin"
-         pathadd "$HOME/.local/sdkconfig"
-
-         # install packages blocked by 2s
-         # hack: use nixGL to determine whether to install packages
-         if [ $(dpkg-query -W -f='$\{Status\}' nixGL 2>/dev/null | grep -c "ok installed") -ne 0 ]; then
-             gssproxy2 -e nix-env --file https://github.com/catern/nix-utils/archive/master.tar.gz --install
-             gssproxy2 nix-env --file https://github.com/guibou/nixGL/archive/main.tar.gz -iA auto.nixGLDefault
-         fi
-      fi
-
       function flame() {
           perf record -g --call-graph fp -- g++
           perf script | stackcollapse-perf.pl > out.perf-folded;
@@ -316,12 +250,11 @@ in {
   };
 
   programs.home-manager = {
-    enable = true;
+    enable = false;
   };
 
   programs.fzf = {
     enable = true;
-    #enableZshIntegration = true;
   };
 
   programs.tmux = {
@@ -333,73 +266,4 @@ in {
   # home.file.".config/scripts".source = ./files/scripts;
   # home.file.".config/scripts".recursive = true;
   # home.sessionPath = ["$HOME/.config/scripts" "$HOME/dotfiles/.bin"];
-    nixpkgs.overlays = [
-    (self: super: {
-      libGL =
-        let
-          inherit (self) lib stdenv libglvnd testers;
-          inherit (self.darwin.apple_sdk.frameworks) OpenGL;
-        in
-        stdenv.mkDerivation (finalAttrs: {
-          pname = "libGL";
-          version = if stdenv.hostPlatform.isDarwin then "4.1" else libglvnd.version;
-          outputs = [ "out" "dev" ];
-
-          # On macOS, libglvnd is not supported, and mesa no longer builds (but it only
-          # provided a software renderer anyway). Provide the OpenGL framework as well
-          # as a pkg-config file for OpenGL.framework.
-          # GLX is not supported nor is OpenGL ES.
-          buildCommand = if stdenv.hostPlatform.isDarwin then ''
-            mkdir -p $out/nix-support $dev/lib/pkgconfig $dev/nix-support
-            echo ${OpenGL} >> $out/nix-support/propagated-build-inputs
-            echo "$out" > $dev/nix-support/propagated-build-inputs
-            mkdir -p $dev/include
-            ln -s ${OpenGL}/Library/Frameworks/OpenGL.framework/Headers $dev/include/GL
-            cat <<EOF >$dev/lib/pkgconfig/gl.pc
-          Name: gl
-          Description: gl library
-          Version: 4.1
-          Libs: -F${OpenGL} -framework OpenGL
-          Cflags: -F${OpenGL} -DAPIENTRY=GLAPIENTRY
-          EOF
-          ''
-
-          # Otherwise, setup gl stubs to use libglvnd.
-          else ''
-            mkdir -p $out/nix-support
-            ln -s ${libglvnd.out}/lib $out/lib
-            mkdir -p $dev/{,lib/pkgconfig,nix-support}
-            echo "$out ${libglvnd} ${libglvnd.dev}" > $dev/nix-support/propagated-build-inputs
-            ln -s ${libglvnd.dev}/include $dev/include
-            genPkgConfig() {
-              local name="$1"
-              local lib="$2"
-              cat <<EOF >$dev/lib/pkgconfig/$name.pc
-            Name: $name
-            Description: $lib library
-            Version: ${libglvnd.version}
-            Libs: -L${libglvnd.out}/lib -l$lib
-            Cflags: -I${libglvnd.dev}/include
-            EOF
-            }
-            genPkgConfig gl GL
-            genPkgConfig egl EGL
-            genPkgConfig glesv1_cm GLESv1_CM
-            genPkgConfig glesv2 GLESv2
-          '';
-
-          passthru.tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
-
-          meta = {
-            description = "Stub bindings using " + (if stdenv.hostPlatform.isDarwin then "OpenGL.framework" else "libglvnd");
-            pkgConfigModules = [ "gl" ] ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ "egl" "glesv1_cm" "glesv2" ];
-          } // (if stdenv.hostPlatform.isDarwin
-            then { inherit (OpenGL.meta) homepage platforms; }
-            else { inherit (libglvnd.meta) homepage license platforms; });
-        });
-    })
-    (self: super: {
-      fcitx-engines = pkgs.fcitx5;
-    })
-  ];
 }
